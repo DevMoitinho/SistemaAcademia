f68e8f7b6234359403ed400dd4275a83f9c7e966
DevMoitinho <jotavmb@gmai.com>
2025-06-05 19:35:11 -0300

    Adição de metodos em Aluno

--- src/java/Aluno.java
+++ src/java/Aluno.java
@@ -1,9 +1,63 @@
 package java;
 
 public class Aluno extends User {
+	
+	public String email;
+	public int idade;
+	public int faltas;
+	public float peso;
+	public float altura;
+	public Treino treino;
 
 	public Aluno(String nome, String senha, int matricula) {
 		super(nome, senha, matricula);
+	}
+	
+	public String infoAluno() {
+		String out = "Nome: "+ nome+ "\n"+
+				"Idade: "+ idade+ "\n"+
+				"Email: "+ email+ "\n"+
+				"Faltas: "+ faltas+ "\n"+
+				"Altura: "+ altura+ "\n"+
+				"Peso: "+ peso+ "\n";
+		
+		return out;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	public float getAltura() {
+		return altura;
+	}
+
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+	
+	public Treino getTreino() {
+		return treino;
 	}
 
 }
