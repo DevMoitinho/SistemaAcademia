141eeb71a281e8aadbdc844fed608965f0d47fbd
DevMoitinho <jotavmb@gmai.com>
2025-08-24 23:30:20 -0300

    grandes mudanças

--- bin/web/javaScript/userScript.js
+++ bin/web/javaScript/userScript.js
@@ -1,4 +1,31 @@
 const login = document.getElementById("sair");
+const user = document.getElementById("userB");
+const treino = document.getElementById("treinoB");
+
 login.addEventListener("click", function(e){
     window.location.href="login.html";
+});
+
+treino.addEventListener("click", function(e){
+    const userPanel = document.getElementById("person");
+    const treinoPanel = document.getElementById("treino");
+    //let faltaPanel = document.querySelector(".falta");
+
+    userPanel.classList.remove('visible');
+    userPanel.classList.add('hidden');
+    
+    treinoPanel.classList.remove('hidden');
+    treinoPanel.classList.add('visible');
+});
+
+user.addEventListener("click", function(e){
+    const userPanel = document.getElementById("person");
+    const treinoPanel = document.getElementById("treino");
+    //let faltaPanel = document.querySelector(".falta");
+
+    treinoPanel.classList.remove('visible');
+    treinoPanel.classList.add('hidden');
+
+    userPanel.classList.remove('hidden');
+    userPanel.classList.add('visible');
 });
